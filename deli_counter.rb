aa776b79480f1f18ad91087e8ff0bff08f311736
Algernon Soriano <alger.soriano@gmail.com>
2022-11-22 22:38:35 +1000

    deli_counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def line(array)
+    if array == []
+    puts "The line is currently empty."
+    else 
+        x = 0
+        names = []
+        while x < array.length
+        names.push("#{x + 1}. #{array[x]}")
+        x += 1
+        end
+    puts "The line is currently: #{names.join(" ")}"
+    end
+end
+a = ["Alger", "Angel"]
+# line(a)
+def take_a_number(array, name)
+    if array == []
+        puts "Welcome, #{name}. You are number 1 in line."
+    else
+        puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+    end
+    array.push(name)
+end
+
+# puts take_a_number(a, "Aundrea")
+def now_serving(array)
+    if array == []
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
